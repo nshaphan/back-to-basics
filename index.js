53c5a2f8bfab719215a09d94b6d093117c2c9ea7
shaphan <shaphan.nzabonimana@andela.com>
2019-10-10 14:31:41 +0200

    setting up environment

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-var myObj = {
-    "one": 1,
-    "Two": 2,
-    "Three": 3
-  }
-  
-  var keys = Object.keys(myObj);
-  
-  var timestwo = keys.map(function (key){
-    return myObj[keys] *2 ;
-  });
-  
-  console.log(timestwo);
+let a = 20;
